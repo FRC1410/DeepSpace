f1a379d80b144644c250fb3e1a14beddca95ccb4
RandyNgo02 <35178937+RandyNgo02@users.noreply.github.com>
2019-03-07 22:28:57 -0600

    janky elevator

---
+++ DeepSpace OKOK Practice/src/main/include/commands/Drive.h
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Command.h>
+#include <frc/Timer.h>
+
+class Drive : public frc::Command {
+  private:
+    double left_stick_amount, right_stick_amount;
+    bool invert_driving, invert_button_was_pressed;
+    
+    double previous_distance, previous_angle;
+    double previous_x[25], previous_z[25], previous_angles[25], previous_times[25];
+    int search;
+
+    double PID_value, limelight_PID_value;
+    double limelight_x, limelight_angle;
+    double latency_x, latency_z, latency_angle;
+    bool target_found, vision_button_was_pressed;
+
+    double invert_timer, previous_timer, vision_timer;
+    frc::Timer m_timer;
+
+  public:
+    Drive();
+    void Initialize() override;
+    void Execute() override;
+    bool IsFinished() override;
+    void End() override;
+    void Interrupted() override;
+};
---
+++ DeepSpace OKOK Practice/src/main/include/commands/ElevatorRun.h
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Command.h>
+#include <frc/Timer.h>
+
+#include "RobotMap.h"
+
+class ElevatorRun : public frc::Command {
+  private:
+    double elevator_input, height;
+
+    bool manual_control, acceleration_complete;
+
+    bool low_button_was_pressed, mid_button_was_pressed, high_button_was_pressed;
+    bool loading_station_button_was_pressed, reset_button_was_pressed;
+    int previous_profile, fail_count;
+
+    frc::Timer m_timer;
+    double previous_timer;
+    
+  public:
+    ElevatorRun();
+    void Initialize() override;
+    void Execute() override;
+    bool IsFinished() override;
+    void End() override;
+    void Interrupted() override;
+};
