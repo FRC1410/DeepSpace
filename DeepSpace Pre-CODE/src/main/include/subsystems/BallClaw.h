2a6f6a6f06fee45337f694e73b2cae07daf999f0
RandyNgo02 <35178937+RandyNgo02@users.noreply.github.com>
2019-03-12 22:50:57 -0600

    Added Compressor Limiter

---
+++ DeepSpace Pre-CODE/src/main/include/subsystems/BallClaw.h
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Subsystem.h>
+#include <frc/WPILib.h>
+#include <ctre/Phoenix.h>
+#include <AHRS.h>
+
+#include "RobotMap.h"
+
+class BallClaw : public frc::Subsystem {
+  private:
+    WPI_TalonSRX m_left_wheel{ball_claw_left};
+    WPI_TalonSRX m_right_wheel{ball_claw_right};
+
+    DigitalInput * ball_limit_switch = new DigitalInput(claw_limit_switch);
+
+  public:
+    BallClaw();
+    void InitDefaultCommand() override;
+    
+    void RunWheels(double left, double right);
+    bool GetLimitSwitch();
+};
